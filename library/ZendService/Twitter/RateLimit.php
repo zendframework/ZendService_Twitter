574aa121a80a5fb89a32de04d08a365f43301a6d
Cal Evans <cal@calevans.com>
2016-08-01 15:18:23 -0400

    Added new RateLimit object. Updated Unit tests to test

--- library/ZendService/Twitter/RateLimit.php
+++ library/ZendService/Twitter/RateLimit.php
@@ -2,24 +2,31 @@
 
 namespace ZendService\Twitter;
 
+use Zend\Http\Headers as Headers;
+
 class RateLimit
 {
-    private $limit = 0;
-    private $remaining = 0;
-    private $reset = 0;
+    private $limit;
+    private $remaining;
+    private $reset;
 	
-	public function __construct($headers)
+	public function __construct(Headers $headers = null)
 	{
-        $this->limit = isset($headers['x-rate-limit-limit']) ? $headers['x-rate-limit-limit'] : 0;
-        $this->remaining = isset($headers['x-rate-limit-remaining']) ? $headers['x-rate-limit-remaining'] : 0;
-        $this->reset = isset($headers['x-rate-limit-reset']) ? $headers['x-rate-limit-reset'] : 0;
+
+        if (! is_null($headers)) {
+	        $headersArray = $headers->toArray();
+	        $this->limit = isset($headersArray['x-rate-limit-limit']) ? $headersArray['x-rate-limit-limit'] : 0;
+	        $this->remaining = isset($headersArray['x-rate-limit-remaining']) ? $headersArray['x-rate-limit-remaining'] : 0;
+	        $this->reset = isset($headersArray['x-rate-limit-reset']) ? $headersArray['x-rate-limit-reset'] : 0;
+        }
 
 	}
 
+
 	public function __get($key)
 	{
-		if exists ($this>$key) {
-			return $this->$key
+		if (isset($this->$key)) {
+			return $this->$key;
 		}
 
 		return;
