268751fb15cb36137a8172326d4f2539e95f531a
Cal Evans <cal@calevans.com>
2016-08-01 15:35:38 -0400

    Documented RateLimit

--- library/ZendService/Twitter/RateLimit.php
+++ library/ZendService/Twitter/RateLimit.php
@@ -1,15 +1,32 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Service
+ */
 
 namespace ZendService\Twitter;
 
 use Zend\Http\Headers as Headers;
 
+/**
+ * Representation of the Rate Limit Headers from Twitter.
+ *
+ */
 class RateLimit
 {
     private $limit;
     private $remaining;
     private $reset;
 	
+    /**
+     * Constructor
+     *
+     * @param  null|Headers $headers
+     */
 	public function __construct(Headers $headers = null)
 	{
 
@@ -23,12 +40,17 @@ class RateLimit
 	}
 
 
+    /**
+     * Retun the requested property
+     *
+     * @return null|integer
+     */
 	public function __get($key)
 	{
 		if (isset($this->$key)) {
 			return $this->$key;
 		}
 
-		return;
+		return null;
 	}
 }
